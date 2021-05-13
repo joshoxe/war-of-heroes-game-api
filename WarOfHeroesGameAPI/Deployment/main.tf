79986af117c9c6d2741a995e4212fe6c95890b50
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-05-13 16:25:08 +0100

    Split out terraform

--- WarOfHeroesGameAPI/Deployment/main.tf
+++ WarOfHeroesGameAPI/Deployment/main.tf
@@ -15,28 +15,15 @@ backend "azurerm" {
   }
 }
 
-variable "username" {
-  type        = string
-  sensitive = true
-}
-variable "password" {
-  type        = string
-  sensitive = true
-}
-
 provider "azurerm" {
   features {}
 }
 
 resource "azurerm_resource_group" "rg" {
-  name     = "warOfHeroes"
+  name     = "warOfHeroesHeroes"
   location = "UK South"
 }
 
-resource "azurerm_resource_group" "db_rg" {
-  name     = "warOfHeroesDBs"
-  location = "UK South"
-}
 
 resource "azurerm_app_service_plan" "app_service_plan" {
     name                = "warOfHeroes-appservice"
@@ -44,19 +31,8 @@ resource "azurerm_app_service_plan" "app_service_plan" {
     resource_group_name = azurerm_resource_group.rg.name
 
     sku {
-        tier = "Standard"
-        size = "S1"
-    }
-}
-
-resource "azurerm_app_service" "users_app_service" {
-    name                = "warOfHeroesUsers"
-    location            = azurerm_resource_group.rg.location
-    resource_group_name = azurerm_resource_group.rg.name
-    app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-    https_only = true
-    site_config {
-        windows_fx_version = "DOTNETCORE|3.1"
+        tier = "Shared"
+        size = "D1"
     }
 }
 
@@ -70,39 +46,14 @@ resource "azurerm_app_service" "heroes_app_service" {
         windows_fx_version = "DOTNETCORE|3.1"
     }
 }
-  
-resource "azurerm_app_service" "frontend_app_service" {
-  name                = "warOfHeroes"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  app_service_plan_id = azurerm_app_service_plan.app_service_plan.id
-  https_only = true
-  site_config {
-      windows_fx_version = "DOTNETCORE|3.1"
-      default_documents = ["index.html"]
-  }
-}
-
-resource "azurerm_sql_server" "user_db" {
-  name                         = "UsersDb"
-  resource_group_name          = azurerm_resource_group.db_rg.name
-  location                     = azurerm_resource_group.db_rg.location
-  version                      = "12.0"
-  administrator_login          = var.username
-  administrator_login_password = var.password
-
-  tags = {
-    environment = "production"
-  }
-}
 
 resource "azurerm_sql_server" "heroes_db" {
   name                         = "HeroesDb"
-  resource_group_name          = azurerm_resource_group.db_rg.name
-  location                     = azurerm_resource_group.db_rg.location
+  resource_group_name          = azurerm_resource_group.rg.name
+  location                     = azurerm_resource_group.rg.location
   version                      = "12.0"
-  administrator_login          = var.username
-  administrator_login_password = var.password
+  administrator_login          = var.DB_USERNAME
+  administrator_login_password = var.DB_PASSWORD
 
   tags = {
     environment = "production"
