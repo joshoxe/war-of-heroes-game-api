c3ab5f7d6fc88276731e7e60fa7a3347519b91a2
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-19 09:57:56 +0100

    Add data changes to EF

---
+++ WarOfHeroesGameAPI/Data/Entities/Ability.cs
@@ -0,0 +1,8 @@
+﻿namespace WarOfHeroesGameAPI.Data.Entities
+{
+    public class Ability
+    {
+        public string Type { get; set; }
+        public int Amount { get; set; }
+    }
+}
--- WarOfHeroesGameAPI/Data/Entities/Hero.cs
+++ WarOfHeroesGameAPI/Data/Entities/Hero.cs
@@ -4,9 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public int AttackDamage { get; set; }
-        public string UltimateAttackName { get; set; }
-        public int UltimateAttackDamage { get; set; }
+        public Ability Ability { get; set; }
         public string Description { get; set; }
     }
 }
