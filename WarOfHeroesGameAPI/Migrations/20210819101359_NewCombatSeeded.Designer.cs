e1683eb4595ffa7cdfb234c762d82a31b2a2cc71
Josh Oxenham <Ajoshua@engineering.ukho.gov.uk>
2021-08-19 11:15:12 +0100

    Migrate database

--- WarOfHeroesGameAPI/Migrations/20210819101359_NewCombatSeeded.Designer.cs
+++ WarOfHeroesGameAPI/Migrations/20210819101359_NewCombatSeeded.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WarOfHeroesGameAPI.Data;
 
 namespace WarOfHeroesGameAPI.Migrations
 {
     [DbContext(typeof(HeroContext))]
-    partial class HeroContextModelSnapshot : ModelSnapshot
+    [Migration("20210819101359_NewCombatSeeded")]
+    partial class NewCombatSeeded
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
